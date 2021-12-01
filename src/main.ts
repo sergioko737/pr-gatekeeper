40c177aeb022f03e3553cb85b5044f67f398959d
sergioko737 <s29585476@gmail.com>
2021-12-01 02:41:49 +0200

    debug output

--- src/main.ts
+++ src/main.ts
@@ -104,7 +104,7 @@ async function run(): Promise<void> {
     if ( context.eventName == 'pull_request' ) {
       console.log(`We are going to request someones approval!!!`)
       // assignReviewers(octokit, reviewer_persons, reviewer_teams, pr_number)
-      await client.pulls.requestRevivers({
+      await client.pulls.requestReviewers({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         pull_number: pr_number,
